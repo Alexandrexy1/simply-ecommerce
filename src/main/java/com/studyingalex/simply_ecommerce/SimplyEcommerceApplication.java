8c20764259b9d64e4a9b86f61cf4c94aa448a638
Alexandre <alexandrenas2003@gmail.com>
2025-07-15 08:21:07 -0300

    first commit

---
+++ src/main/java/com/studyingalex/simply_ecommerce/SimplyEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.studyingalex.simply_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public final class SimplyEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyingalex/simply_ecommerce/SimplyEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyingalex.simply_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
